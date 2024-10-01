6f6a21d795d5292bb9466f3254d664aaf3edd0a6
oman <smithowain@hotmail.com>
2024-10-01 22:33:17 +0900

    Built a function that makes the playing card grid

--- javascript.js
+++ javascript.js
@@ -1,48 +1,30 @@
-const chkBtn = document.querySelector("#check-btn");
-const inputTxtBx = document.querySelector('#text-input');
-const resultsDiv = document.querySelector("div");
-let revTextResults = "";
+const gameBox = document.querySelector("#gameBox");
 
-function noValue () {
-  var inputTxt = "";
-  var inputTxt = inputTxtBx.value;
-  if (inputTxt === "") {
-    alert("Please input a value");
-  } else {
-    reverseTxt();
+// First I will need to create a grid of cards with different ids
+
+function gridCreator () {
+  for (let i=0; i<12; i++) {
+    let div = document.createElement("div");
+    div.style.width="100px";
+    div.style.height="100px";
+    div.style.background="red";
+    div.style.color="white";
+    div.innerHTML="Hello"
+    div.id="card" + i;
+    div.classList.add("playingCards")
+    console.log("hello there")
+    document.getElementById("gameBox").appendChild(div);
   }
-};
-
-//I need a function that will reverse the text by iterating through the text and adding to an array
-
-function reverseTxt (inputTxt) {
-  var inputTxt = inputTxtBx.value;
-  console.log(inputTxt);
-  const reversedArray = [];
-  for (i=1; i<inputTxt.length+1; i++) {
-    reversedArray.push(inputTxt[inputTxt.length-[i]]);
-  }
-  // .tostring places commas between letters, so use .join
-  revTextResults = ignoreSpecialChar(reversedArray.join("").toLowerCase());
-  console.log(revTextResults);
-  palindromeChk();
-};
-
-function ignoreSpecialChar (string) {
-  return string.replace(/[-/:_,\s.*+?^${}()|[\]\\]/g, "");
 }
 
-function palindromeChk () {
-  var inputTxt = inputTxtBx.value;  
-  var inputTxtToChk = ignoreSpecialChar(inputTxtBx.value.toLowerCase());
-  if (inputTxtToChk === revTextResults) {
-    resultsDiv.innerText = (`${inputTxt} is a palindrome`);
-  } else {
-    resultsDiv.innerText = (`${inputTxt} is not a palindrome`);
-  }
+gridCreator()
 
-};
+// then I will need to randomize the playing cards
+
+// then I will need to match the ids to pictures
 
 
-// add the event listener with a callback function
-chkBtn.addEventListener('click', noValue);
+
+// then I will need to make the cards turn over when they have been clicked
+
+// if they match then the player scores a point
